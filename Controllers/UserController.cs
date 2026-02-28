b3c23b435eef68d9445b6327b2e22d605973dfda
DESKTOP-KU34JC9 <thongminhtranle123@gmail.com>
2026-02-28 19:35:42 +0700

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DoAnWebBanDoChoi.Data;
+
+namespace DoAnWebBanDoChoi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
