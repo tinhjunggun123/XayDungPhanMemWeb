a849fcd3c2c19d10b7d177475e14c16da87fe80e
DESKTOP-KU34JC9 <thongminhtranle123@gmail.com>
2026-02-28 20:06:09 +0700

    Update program

--- Program.cs
+++ Program.cs
@@ -24,11 +24,9 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors("AllowAll");
 //app.UseHttpsRedirection();
