a055d34f554bd0df0fd863e4e588e24719eb6916
DESKTOP-KU34JC9 <thongminhtranle123@gmail.com>
2026-02-28 21:57:35 +0700

    Update program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-using DoAnWebBanDoChoi.Data;
+﻿using DoAnWebBanDoChoi.Data;
+using DoAnWebBanDoChoi.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,5 +36,19 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
+
+    if (!db.Users.Any())
+    {
+        db.Users.AddRange(
+            new User { Name = "Lê Trần Minh Thông", Email = "DH52201514@student.stu.edu.vn", Phone = "0900000001" },
+            new User { Name = "Nguyễn Thanh Phú", Email = "DH52201223@student.stu.edu.vn", Phone = "0900000002" },
+            new User { Name = "Ngô Hoàng Nam", Email = "DH52201077@student.stu.edu.vn", Phone = "0900000003" },
+            new User { Name = "Hoàng Anh Quân", Email = "DH52201284@student.stu.edu.vn", Phone = "0900000003" },
+            new User { Name = "Ngô Thái Khang", Email = "DH52201462@student.stu.edu.vn", Phone = "0900000003" },
+            new User { Name = "Nguyễn Trường Thành", Email = "DH52200838@student.stu.edu.vn", Phone = "0900000003" }
+        );
+
+        db.SaveChanges();
+    }
 }
 app.Run();
